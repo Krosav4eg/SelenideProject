79bb073bd82b32374f5f854030f91a50ebc69240
Sergey Potapov <sergeypotapoff12@gmail.com>
2022-09-11 17:47:57 +0300

    selector noteBooksCategories was changed to ElementsCollection

--- src/main/java/fragments/MenuCategoriesFragment.java
+++ src/main/java/fragments/MenuCategoriesFragment.java
@@ -1,20 +1,22 @@
 package fragments;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static helpers.ActionHelper.clickOnButton;
 
 public class MenuCategoriesFragment  {
     SelenideElement allMenuCategories = $(byCssSelector("ul.menu-categories.ng-star-inserted"));
-    SelenideElement noteBooksCategories = $(byXpath("(//a[contains(text(),'Ноутбуки')])[3]"));
+    ElementsCollection noteBooksCategories = $$(byXpath("//a[contains(text(),'Ноутбуки')]"));
 
 
     @Step("Click on notebooks categories")
     public void clickOnNoteBooksCategories(){
-        clickOnButton(noteBooksCategories);
+        clickOnButton(noteBooksCategories.get(2));
     }
 }
