0d53784c35dd3f174917634ce623207e27e885de
Sergey Potapov <sergeypotapoff@12gmail.com>
2022-11-27 13:11:11 +0200

    RestAssured.filters was changed to static because of duplication of authorization

--- src/test/java/autotests/api/BaseTest.java
+++ src/test/java/autotests/api/BaseTest.java
@@ -3,7 +3,6 @@ package autotests.api;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import lombok.extern.log4j.Log4j2;
-import org.testng.annotations.BeforeTest;
 
 @Log4j2
 public class BaseTest {
@@ -11,8 +10,7 @@ public class BaseTest {
     protected static final String PATH_TO_CREATION_BOOKING_RESPONSE_SCHEMA = "json/schemas/CreateBookingSchema.json";
     protected static final String PATH_TO_GENERAL_BOOKING_INFO_SCHEMA = "json/schemas/BookingInfoSchema.json";
 
-    @BeforeTest(alwaysRun = true)
-    public void setFilter() {
+    static {
         RestAssured.filters(new AllureRestAssured());
     }
 }
