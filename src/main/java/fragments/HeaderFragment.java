6b8b27d3aeace170132b31b91a78247e6971c595
Sergey Potapov <sergeypotapoff@12gmail.com>
2023-01-21 23:04:47 +0200

    was added navigateToNotebooksCategory

--- src/main/java/fragments/HeaderFragment.java
+++ src/main/java/fragments/HeaderFragment.java
@@ -2,10 +2,9 @@ package fragments;
 
 import com.codeborne.selenide.SelenideElement;
 
-
 import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selenide.$;
-
+import static driver.WebElementInteractions.isVisible;
 import static helpers.ActionHelper.clickOnButton;
 
 public class HeaderFragment {
@@ -14,8 +13,11 @@ public class HeaderFragment {
     SelenideElement inputSearchForm = $(byCssSelector("input[name='search']"));
     SelenideElement findButton = $(byCssSelector("button.button_color_green"));
     SelenideElement shoppingCardButton = $(byCssSelector("use[href='#icon-header-basket']"));
+    SelenideElement catalogMenuList = $(byCssSelector("div.menu__hidden-content.ng-star-inserted"));
 
     public void clickOnCatalogMenuButton() {
         clickOnButton(catalogMenuButton);
+        isVisible(catalogMenuList);
+
     }
 }
