431081408bcb4bcf58497ac28ed811d26ba9d3a8
Sergey Potapov <sergeypotapoff@12gmail.com>
2023-06-19 11:21:52 +0300

    was added Allure Env Creator.

---
+++ src/test/java/autotests/ui/BaseTestUtils.java
@@ -0,0 +1,27 @@
+package autotests.ui;
+
+import com.github.rsheremeta.AllureEnv;
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import org.aeonbits.owner.ConfigFactory;
+import utils.PropsConfig;
+
+import java.util.Map;
+
+@Log4j2
+public class BaseTestUtils {
+
+    public static final PropsConfig PROPS = ConfigFactory.create(PropsConfig.class);
+
+    /**
+     * Current method necessary for setting up information about testing environment.
+     */
+    @Step("Set all detailed information about Environment")
+    protected static synchronized void setEnvironmentValuesForAllure() {
+        Map<String, String> envData = Map.of("Browser", PROPS.BASE_BROWSER(), "Browser.Version", "111.0, (64 bit)",
+                "Base UI URL", PROPS.BASE_URL(), "Base API URL", PROPS.BASE_API_URL());
+        AllureEnv.createAllureEnvironmentFile(envData);
+        envData.clear();
+        log.info("***** Allure environmental data has been gathered successfully *****");
+    }
+}
