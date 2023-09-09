d5d58f00ff590336f680fe1fd36150e563774f2a
Sergey Potapov <sergeypotapoff12@gmail.com>
2023-09-10 00:19:44 +0300

    small fixes of checkNameOfSelectedProductFormFilterBar()

--- src/main/java/ui/fragments/FilterSideFragment.java
+++ src/main/java/ui/fragments/FilterSideFragment.java
@@ -1,6 +1,7 @@
 package ui.fragments;
 
 import com.codeborne.selenide.SelenideElement;
+import lombok.SneakyThrows;
 import ui.helpers.ActionHelper;
 
 import static com.codeborne.selenide.Selectors.byCssSelector;
@@ -12,9 +13,11 @@ public class FilterSideFragment {
     SelenideElement closeCrossPopUpButton = $(byCssSelector("span.exponea-close-cross"));
     String sidebarBrand = "//ul[@class='checkbox-filter']//a[contains(@data-id, '%s')]";
 
+    @SneakyThrows
     public FilterSideFragment selectBrandFromSideBar(String brandName) {
         ActionHelper.isElementDisplayed(filterBarBody);
         ActionHelper.clickOnButton(closeCrossPopUpButton);
+        Thread.sleep(3000);
         setRequiredTextIntoXpath(sidebarBrand, brandName).click();
         ActionHelper.isElementSelected(setRequiredTextIntoXpath(sidebarBrand, brandName));
         return new FilterSideFragment();
