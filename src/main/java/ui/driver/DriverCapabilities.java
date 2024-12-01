f5b3edb26afb8845848c975d4a15af92395cc433
Sergey Potapov <sergeypotapoff12@gmail.com>
2024-12-01 13:21:33 +0100

    was added proxy

--- src/main/java/ui/driver/DriverCapabilities.java
+++ src/main/java/ui/driver/DriverCapabilities.java
@@ -1,5 +1,6 @@
 package ui.driver;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -7,13 +8,15 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class DriverCapabilities {
 
     public static ChromeOptions getChromeOptions() {
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("myproxy:8080");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         options.addArguments("--disable-notifications");
-        options.addArguments("--disable-javascript");
         options.addArguments("disable-infobars");
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setCapability("proxy", proxy);
         options.setExperimentalOption("useAutomationExtension", false);
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         return options;
