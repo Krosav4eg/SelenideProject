4a6ac24c19bc370a9d61313864b3c4e76c05d402
Sergey Potapov <sergeypotapoff12@gmail.com>
2026-07-17 23:06:23 +0200

    was changed path to all tests in Regression suite. Now only API tests will be executed.

--- src/test/java/autotests/api/BaseTest.java
+++ src/test/java/autotests/api/BaseTest.java
@@ -1,20 +1,52 @@
 package autotests.api;
 
 import api.helper.ConsoleLoggingFilter;
+import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import lombok.extern.log4j.Log4j2;
+import org.aeonbits.owner.ConfigFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
+import ui.listeners.AllureListener;
+import utils.PropsConfig;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @Log4j2
+@Listeners({AllureListener.class})
 public class BaseTest {
 
     protected static final String PATH_TO_CREATION_BOOKING_RESPONSE_SCHEMA = "json/schemas/CreateBookingSchema.json";
     protected static final String PATH_TO_GENERAL_BOOKING_INFO_SCHEMA = "json/schemas/BookingInfoSchema.json";
     public static final ThreadLocal<String> currentTestName = new ThreadLocal<>();
+    public static final PropsConfig PROPS = ConfigFactory.create(PropsConfig.class);
+
+
+    @BeforeSuite
+    @Step("Set all detailed information about Environment")
+    public void setAllureEnvironment() {
+        String env = "Browse.Name=" + PROPS.BASE_BROWSER() + "\n" +
+                "Browser.Version=142.0.7444.60  (64 bit)\n" +
+                "OS=Windows 11\n" +
+                "BASE_UI_URL=" + PROPS.BASE_URL() + "\n" +
+                "BASE_API_URL=" + PROPS.BASE_API_URL() + "\n";
+        try {
+            Path path = Paths.get("target/allure-results/environment.properties");
+            Files.createDirectories(path.getParent());
+            Files.writeString(path, env, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            System.out.println("Allure environment.properties created successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @BeforeMethod
     public void beforeMethod(Method method) {
--- src/test/java/autotests/ui/BaseTest.java
+++ src/test/java/autotests/ui/BaseTest.java
@@ -1,22 +1,17 @@
 package autotests.ui;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.*;
-import ui.browserfactory.BrowserFactoryProvider;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import io.qameta.allure.Step;
-import ui.listeners.AllureListener;
 import lombok.extern.log4j.Log4j2;
 import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import ui.browserfactory.BrowserFactoryProvider;
+import ui.listeners.AllureListener;
 import ui.pages.NoteBooksPage;
 import utils.PropsConfig;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 import static com.codeborne.selenide.Selenide.open;
 import static ui.browserfactory.DriverManager.quitDriver;
@@ -27,23 +22,23 @@ public class BaseTest {
     public static final PropsConfig PROPS = ConfigFactory.create(PropsConfig.class);
     NoteBooksPage noteBooksPage = new NoteBooksPage();
 
-    @BeforeSuite
-    @Step("Set all detailed information about Environment")
-    public void setAllureEnvironment() {
-        String env = "Browse.Name=" + PROPS.BASE_BROWSER() + "\n" +
-                "Browser.Version=142.0.7444.60  (64 bit)\n" +
-                "OS=Windows 11\n" +
-                "BASE_UI_URL=" + PROPS.BASE_URL() + "\n" +
-                "BASE_API_URL=" + PROPS.BASE_API_URL() + "\n";
-        try {
-            Path path = Paths.get("target/allure-results/environment.properties");
-            Files.createDirectories(path.getParent());
-            Files.writeString(path, env, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-            System.out.println("Allure environment.properties created successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    @BeforeSuite
+//    @Step("Set all detailed information about Environment")
+//    public void setAllureEnvironment() {
+//        String env = "Browse.Name=" + PROPS.BASE_BROWSER() + "\n" +
+//                "Browser.Version=142.0.7444.60  (64 bit)\n" +
+//                "OS=Windows 11\n" +
+//                "BASE_UI_URL=" + PROPS.BASE_URL() + "\n" +
+//                "BASE_API_URL=" + PROPS.BASE_API_URL() + "\n";
+//        try {
+//            Path path = Paths.get("target/allure-results/environment.properties");
+//            Files.createDirectories(path.getParent());
+//            Files.writeString(path, env, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+//            System.out.println("Allure environment.properties created successfully.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     @BeforeClass(alwaysRun = true)
     public void mainSteps() {
